c407ce59c9a135a2181a8bc0621a30bc5454ca5d
Xingyu Liang <62229187+cqqjlxy@users.noreply.github.com>
2024-10-09 02:45:17 +0200

    Create env.js

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://pbe.echon.top/'
+window.settings = {
+  // 站点名称
+  title: 'Echon Servers',
+  // 站点描述
+  description: 'Echon Servers',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'black', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
